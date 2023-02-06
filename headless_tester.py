67a23162707f951e3e252dc2c4283757548c3f30
Noah Syler <99105291+NoahSyler@users.noreply.github.com>
2023-02-06 17:02:06 -0500

    Update headless_tester.py

--- headless_tester.py
+++ headless_tester.py
@@ -1,4 +1,4 @@
-#import unittest
+
 from selenium import webdriver
 from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver.support.select import Select
@@ -40,19 +40,19 @@ def png_screenshot(url, driver, toggles):
 #				(depending on which toggle options have been set to True)
 
 def open_link(url, driver, toggles):
+
 	past = datetime.now()
 	driver.get(url)
 	present = datetime.now()
 	ttl = present-past
 	if ("ErrorException" in driver.page_source) or ("Error" in driver.page_source):
 		print(f"{url[37:]} has failed us")
-		print(f"{option_url[37:]} has failed us")
 		print(driver.find_element(By.TAG_NAME, "a").get_attribute('innerHTML'))
 		print(driver.find_element(By.TAG_NAME, "a").get_attribute('outerHTML'))
-		print(dir(driver.find_element(By.TAG_NAME, "a")))
-		print(vars(driver.find_element(By.TAG_NAME, "a")))		
+		#print(dir(driver.find_element(By.TAG_NAME, "a")))
+		#print(vars(driver.find_element(By.TAG_NAME, "a")))		
 		failures[url] = "error"
-		with open(f"{toggles['txt_folder']}{url[37:]}_error.txt", "a") as f:
+		with open(f"{toggles['txt_folder']}{url[37:]}_error.txt", "a", encoding="utf-8") as f:
 			f.write(f"{url} \n\n")
 			f.write(driver.find_element(By.XPATH, "/html/body").text)
 			f.write("\n\n\n\n")	
@@ -64,12 +64,14 @@ def open_link(url, driver, toggles):
 		print(f"{url[37:]} is a success. Time To Load {timedelta.total_seconds(ttl)} seconds")
 	#driver.get(url)
 		if toggles['gather_load_time'] == True:
-			with open(f"{toggles['txt_folder']}success_data.txt", "a") as f:
+			with open(f"{toggles['txt_folder']}success_data.txt", "a", encoding="utf-8") as f:
 				f.write(f"{url} \n\tLOAD TIME: {timedelta.total_seconds(ttl)} seconds\n")
 		if toggles['error_page_only'] == False:		
 			write_console_log(driver, f"{toggles['txt_folder']}success_data.txt", toggles)
+'''
 
 
+'''
 #This is the function called to write the console logs to a text file
 #The toggle option set in toggles{} will determine which logs will be written
 #It begins the entry with "LOG DATA BEGINNING" and ends with "LOG DATA END", to make the beginning and end more easily searchable
@@ -108,36 +110,87 @@ def write_console_log(driver, doc, toggles):
 #		In this case, the if statements "if url == None:" and "if "http" not in url:" are meant to catch them
 
 
-def test_all_links(driver, toggles):
+def test_all_links(driver, toggles, position = 1):
 
 	failures = {}
 	x=1
 	links = driver.find_elements(By.TAG_NAME, "a")
+	#print(links)
+
+
 	for link in links:
-		
+
 		print(f"Progress: {round((x/len(links)*100), 2)}%")
 
-		url =  link.get_attribute("href")
+		try:
+			url =  link.get_attribute("href")
+		except Exception as e:
+			print(driver.current_url)
+			print(f"Exception: {e}")
+			continue	
 		print(url)
 		if url == None:
 			continue
 		if "http" not in url:
 			continue	
+
 		driver.execute_script(f"window.open('');")
 		driver.switch_to.window(driver.window_handles[1])
 
 		try:
 			open_link(url, driver, toggles)
-		
 		except:
 
 			print(f"Unable to load page {url}")	
 			failures[url] = "Unable to load page"
-			with open(f"{toggles['txt_folder']}{url[37:]}_error.txt", "a") as f:
+			with open(f"{toggles['txt_folder']}error_pages.txt", "a", encoding="utf-8") as f:
 				f.write(f"{url} \n\nPAGE DID NOT LOAD\n\n")
 				f.write(driver.find_element(By.XPATH, "/html/body").text)
 				f.write("\n\n\n\n")	
-			png_screenshot(url, driver, toggles)
+			png_screenshot(url, driver, toggles)	
+
+
+
+
+
+		if (toggles['secondary_links'] == True):
+
+			secondary_links = driver.find_elements(By.TAG_NAME, "a")
+
+			for secondary_link in secondary_links:
+				try:
+					secondary_url =  secondary_link.get_attribute("href")					
+				except Exception as e:
+					print(driver.current_url)
+					print(f"Exception: {e}")
+					continue
+				print(secondary_url)
+				if secondary_url == None:
+					continue
+				if "http" not in secondary_url:
+					continue	
+				driver.execute_script(f"window.open('');")
+				driver.switch_to.window(driver.window_handles[2])
+
+				try:
+					open_link(secondary_url, driver, toggles)
+				except:
+		
+					print(f"Unable to load page {secondary_url}")
+					print(f"Link found on {url}")	
+					failures[secondary_url] = "Unable to load page"
+					with open(f"{toggles['txt_folder']}{secondary_url[37:]}error.txt", "a", encoding="utf-8") as f:
+						f.write(f"{secondary_url} \n\nPAGE DID NOT LOAD\n\n")
+						f.write(driver.find_element(By.XPATH, "/html/body").text)
+						f.write("\n\n\n\n")	
+					png_screenshot(secondary_url, driver, toggles)	
+		
+
+				driver.close()
+				driver.switch_to.window(driver.window_handles[1])
+				#print(driver.window_handles)
+
+
 
 		driver.close()
 		driver.switch_to.window(driver.window_handles[0])		
@@ -164,6 +217,20 @@ def login(driver, un, ps):
 
 def main():
 	
+#the toggles keys 'error', 'fatal', 'warn', 'anything_other_than_info', 'info', and 'all_console' will set with console logs will be written
+#		if 'info' = True, or if 'error', 'fatal', or 'warn' have been set to False, then 'anything_other_than_info' will be set to False
+#				this prevents 'anything_other_than_info' from grabbing different logs than requested,
+#				because it is called first in the if statements in write_console_log()
+#
+
+	toggles = {'error':True, 'fatal':True, 'warn':True,'anything_other_than_info':True, 
+	'info':False, 'all_console': False, 'error_page_only': True, 'debug_script': False, 
+	'gather_load_time': False, 'png_folder': 'screenshots\\', 'txt_folder': 'logs\\',
+	'secondary_links': True}
+
+	if not (toggles['error'] and toggles['fatal'] and toggles['warn']) or toggles['info']:
+		toggles['anything_other_than_info'] = False
+
 	d = DesiredCapabilities.EDGE
 	d['ms:loggingPrefs'] = { 'browser':'ALL' }
 	
@@ -178,21 +245,13 @@ def main():
 	
 	driver = webdriver.Edge(capabilities=d)
 
-
+	driver.set_page_load_timeout(30)
+	
 	driver.get("https://<website>")
 
 	login(driver, "<username>", "<password>")
 
-#the toggles keys 'error', 'fatal', 'warn', 'anything_other_than_info', 'info', and 'all_console' will set with console logs will be written
-#		if 'info' = True, or if 'error', 'fatal', or 'warn' have been set to False, then 'anything_other_than_info' will be set to False
-#				this prevents 'anything_other_than_info' from grabbing different logs than requested,
-#				because it is called first in the if statements in write_console_log()
-#
 
-	toggles = {'error':True, 'fatal':True, 'warn':True,'anything_other_than_info':True, 'info':False, 'all_console': False, 'error_page_only': True, 'debug_script': False, 'gather_load_time': False, 'png_folder': 'screenshots\\', 'txt_folder': 'logs\\'}
-
-	if not (toggles['error'] and toggles['fatal'] and toggles['warn']) or toggles['info']:
-		toggles['anything_other_than_info'] = False
 
 	#Checks all links on a webpage to see if they will be route to an error page
 	#Any link that routes to an error page will append that error to a test document.
